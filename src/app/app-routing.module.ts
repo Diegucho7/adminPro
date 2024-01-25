deb0653da9d5868e53538ed46274f16040657d9f
Diegucho7 <87243460+Diegucho7@users.noreply.github.com>
2024-01-25 01:31:35 -0500

    avance en modulos encapsulados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { GraficaComponent } from './pages/grafica/grafica.component';
 import { ProgressComponent } from './pages/progress/progress.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
