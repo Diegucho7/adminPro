f49d49919f4c3d902352f906625e7cf6a11df892
Diegucho7 <87243460+Diegucho7@users.noreply.github.com>
2024-01-27 05:15:41 -0500

    completo el capitulo 7

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+    constructor(private route: Router){
+
+    }
+
+
+  login(){
+    this.route.navigateByUrl('/')
+  }
 }
