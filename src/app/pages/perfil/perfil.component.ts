1f8ebab375d79c9941569f982f2c0d9d43c690b6
Diegucho7 <87243460+Diegucho7@users.noreply.github.com>
2024-02-14 00:28:27 -0500

    avance con imagenes de perfil usuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario.service';
 import { Usuario } from '../../models/usuario.model';
+import { FileUploadService } from '../../services/file-upload.service';
 
 @Component({
   selector: 'app-perfil',
@@ -12,9 +13,12 @@ export class PerfilComponent implements OnInit {
 
   public perfilForm!: FormGroup;
   public usuario!: Usuario;
+  public imagenSubir!: File;
+  public imgTemp: any = null ;
 
   constructor(private fb: FormBuilder,
-              private usuarioService: UsuarioService  
+              private usuarioService: UsuarioService,
+              private fileUploadservice: FileUploadService 
     ){
         this.usuario = usuarioService.usuario;
   }
@@ -39,4 +43,24 @@ export class PerfilComponent implements OnInit {
     ;
   }
 
+  cambiarImagen(file:File){
+   this.imagenSubir = file;
+
+
+   if(!file){return;}
+
+   const reader = new FileReader();
+   const url64 = reader.readAsDataURL(file);
+
+
+   reader.onloadend = () =>{
+    this.imgTemp = reader.result;
+   }
+  }
+
+  subirImagen(){
+    this.fileUploadservice.actualizarFoto(this.imagenSubir, 'usuarios', this.usuario.uid!)
+    .then(img =>this.usuario.img = img);
+  }
 }
+
